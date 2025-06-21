d96470cc1e0f0b2692c69a5be4228d964e31037c
elfabri <navarro.fabricio.nahuel@gmail.com>
2025-06-21 01:19:18 -0300

    indents and clear results

--- unit_test.ts
+++ unit_test.ts
@@ -1,61 +1,65 @@
 // describe what's going to be tested
 export function describe(title: string, func: () => void) {
-    console.log(`-> testing ${title}\n`);
-    func();
+  console.log(`-> testing ${title}\n`);
+  func();
 }
 
 // individual tests
 export function it(title: string, test: () => void) {
-    console.log(title);
-    test();
+  console.log(title);
+  test();
 }
 
 // assert
 export let assert = {
-    strictEqual(
-        actual: number | string,
-        expected: number | string) {
+  strictEqual(
+    actual: number | string,
+    expected: number | string) {
 
-        if (actual !== expected) {
-            console.log(`Assertion failed:\n-expected: ${expected},\n-got: ${actual}`);
-        }
+      if (actual !== expected) {
+        console.log(`Assertion FAIL:\n-expected: ${expected},\n-got: ${actual}`);
+        return;
+      }
+      console.log("PASS");
     },
     deepEqual(
-        actual: (string | number | boolean)[] | string,
-        expected: (string | number | boolean)[] | string) {
+      actual: (string | number | boolean)[] | string,
+      expected: (string | number | boolean)[] | string) {
 
         if (typeof actual !== typeof expected) {
-            console.log(
-                `Assertion failed case 1:\n-expected:\n${expected}\n\n-got:\n${actual}`
-            );
-            return;
+          console.log(
+            `Assertion FAIL case 1:\n-expected:\n${expected}\n\n-got:\n${actual}`
+          );
+          return;
         }
 
         if ((Array.isArray(actual) && Array.isArray(expected)) || typeof actual === "string") {
 
-            if (actual.length !== expected.length) {
-                console.log(
-                    `Assertion failed case 2:\n-expected:\n${expected}\n\n-got:\n${actual}`
-                );
-                console.log("not the same length");
-                console.log(`actual: ${actual.length}`);
-                console.log(`expected: ${expected.length}`);
-                return;
-            }
-            for (let i=0; i < actual.length; i++) {
-                if (actual[i] !== expected[i]) {
-                    console.log(
-                        `Assertion failed case 3:\n-expected:\n${expected}\n\n-got:\n${actual}`
-                    );
-                    return;
-                }
-            }
-        } else {
+          if (actual.length !== expected.length) {
             console.log(
-                `Assertion failed case 4:\n-expected:\n${expected}\n\n-got:\n${actual}`
+              `Assertion FAIL case 2:\n-expected:\n${expected}\n\n-got:\n${actual}`
             );
+            console.log("not the same length");
+            console.log(`actual: ${actual.length}`);
+            console.log(`expected: ${expected.length}`);
+            return;
+          }
+          for (let i=0; i < actual.length; i++) {
+            if (actual[i] !== expected[i]) {
+              console.log(
+                `Assertion FAIL case 3:\n-expected:\n${expected}\n\n-got:\n${actual}`
+              );
+              return;
+            }
+          }
+        } else {
+          console.log(
+            `Assertion FAIL case 4:\n-expected:\n${expected}\n\n-got:\n${actual}`
+          );
+          return;
         }
-    }
+        console.log("PASS");
+      }
 };
 
 // run all tests
